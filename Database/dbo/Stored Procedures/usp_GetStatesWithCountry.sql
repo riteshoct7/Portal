1f000fd0d1dfe67c38b2e68378a414f420ab4a2a
Ritesh <ritesh_rhce@yahoo.co.in>
2021-11-20 13:17:54 +0530

    Added Database changes for stored procedure

---
+++ Database/dbo/Stored Procedures/usp_GetStatesWithCountry.sql
@@ -0,0 +1,20 @@
+﻿-- =============================================
+-- Author:		<Ritesh Parekh>
+-- Create date: <20th Nov 2021>
+-- Description:	<Get list of states with country>
+-- =============================================
+CREATE PROCEDURE usp_GetStatesWithCountry
+	
+	@StateID int,
+	@Enabled  bit
+AS
+BEGIN
+
+select S.StateId,S.StateName,S.StateDescription,S.Enabled,S.CountryId,C.CountryName
+from States S
+left outer join Countries C on S.CountryID = C.CountryID
+where S.StateId = case when (@StateID is null or @StateID =0) then S.StateId else @StateID end
+and S.Enabled  = case when (@Enabled  is null or @Enabled =0) then S.Enabled  else @Enabled end
+order by S.StateId desc
+	
+END
